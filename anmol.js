7c4053fc26756568cbb80d3fc255c0bf239ae215
Anmol Shikhar <anmolshikhar2323@gmail.com>
2025-03-13 21:57:29 +0530

    new_work

---
+++ anmol.js
@@ -0,0 +1,27 @@
+let arr =[3,5,66,7];
+// arr.map((value)=>{
+//     console.log(value);
+//     console.log(value**2);
+// })
+// console.log(arr);
+// arr.forEach((value)=>{
+//     console.log(value**2);
+// })
+// console.log(arr);
+let a;
+// let anmol =["anmol","kannu","mannu","shikhar"];
+// anmol.map((value)=>{
+//     console.log(value.toUpperCase());
+// })
+arr.map((didi)=>{
+   a= didi/2;
+   return a;
+})
+// arr.forEach((ok)=>{
+//  console.log(ok/2);
+  
+// })
+a.forEach((value)=>{
+    console.log(value)
+})
+const users = [{name: "Alice"}, {name: "Bob"}, {name: "Charlie"}];
