bb6310ff762c5677f707550581f483a59aceda8c
Anmol Shikhar <anmolshikhar2323@gmail.com>
2025-04-18 23:49:55 +0530

    new learning

--- anmol.js
+++ anmol.js
@@ -1,27 +1,37 @@
-let arr =[3,5,66,7];
-// arr.map((value)=>{
-//     console.log(value);
-//     console.log(value**2);
+// let arr =[3,5,66,7];
+// // arr.map((value)=>{
+// //     console.log(value);
+// //     console.log(value**2);
+// // })
+// // console.log(arr);
+// // arr.forEach((value)=>{
+// //     console.log(value**2);
+// // })
+// // console.log(arr);
+// let a;
+// // let anmol =["anmol","kannu","mannu","shikhar"];
+// // anmol.map((value)=>{
+// //     console.log(value.toUpperCase());
+// // })
+// arr.map((didi)=>{
+//    a= didi/2;
+//    return a;
 // })
-// console.log(arr);
-// arr.forEach((value)=>{
-//     console.log(value**2);
-// })
-// console.log(arr);
-let a;
-// let anmol =["anmol","kannu","mannu","shikhar"];
-// anmol.map((value)=>{
-//     console.log(value.toUpperCase());
-// })
-arr.map((didi)=>{
-   a= didi/2;
-   return a;
-})
-// arr.forEach((ok)=>{
-//  console.log(ok/2);
+// // arr.forEach((ok)=>{
+// //  console.log(ok/2);
   
+// // })
+// a.forEach((value)=>{
+//     console.log(value)
 // })
-a.forEach((value)=>{
-    console.log(value)
-})
-const users = [{name: "Alice"}, {name: "Bob"}, {name: "Charlie"}];
+// const users = [{name: "Alice"}, {name: "Bob"}, {name: "Charlie"}];
+let url = "https://bobsburgers-api.herokuapp.com/characters/?limit=9&skip=6"
+let imag = document.querySelector("#img");
+const getFacts = async() => {
+    console.log("Fetching data...");
+    let response = await fetch(url);
+    console.log(response);
+    let data = await response.json();
+    console.log(data[0].image);
+    imag.src =(data[0].image);
+}
